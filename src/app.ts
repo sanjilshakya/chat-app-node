2e89e66bc0d204513165c7d14f328010b3c644b8
Sanjil <sanzil99@gmail.com>
2023-12-08 17:16:01 -0500

    project setup

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Express } from "express";
+
+import { SetupServer } from "./setupServer";
+import databaseConnection from "./setupDatabase";
+import { config } from "./config";
+
+class App {
+  public initialize(): void {
+    this.loadConfig();
+    databaseConnection();
+    const app: Express = express();
+    const server: SetupServer = new SetupServer(app);
+    server.start();
+  }
+
+  private loadConfig(): void {
+    config.validateConfig();
+  }
+}
+
+const app: App = new App();
+app.initialize();
